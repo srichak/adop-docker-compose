a3f5bfbe8c497fbaee7d7661ae8fe92799f14491
zrks <marcis.veveris@outlook.com>
2016-10-28 15:00:19 +0000

    Change DN

--- env.config.sh
+++ env.config.sh
@@ -4,8 +4,8 @@
 # LDAP
 
 export LDAP_SERVER="ldap:389"
-export LDAP_DOMAIN="ldap.example.com"
-export LDAP_FULL_DOMAIN="dc=ldap,dc=example,dc=com"
+export LDAP_DOMAIN="adop.accenture.com"
+export LDAP_FULL_DOMAIN="dc=adop,dc=accenture,dc=com"
 export LDAP_USER_BASE_DN="ou=people"
 export LDAP_GROUP_BASE_DN="ou=groups"
 export LDAP_USER_SEARCH="uid={0}"
@@ -14,11 +14,12 @@ export LDAP_ACCOUNTPATTERN='(cn=${username})'
 export LDAP_ACCOUNTFULLNAME='${cn}'
 export LDAP_GROUPPATTERN='(cn=${groupname})'
 export LDAP_GROUPMEMBERPATTERN='(uniqueMember=${dn})'
-export LDAP_MANAGER_DN="cn=admin,dc=ldap,dc=example,dc=com"
+export LDAP_MANAGER_DN="cn=admin,dc=adop,dc=accenture,dc=com"
 export LDAP_GROUP_NAME_ADMIN="administrators"
 
 
 # Gitlab
+
 export GITLAB_ROOT_PASSWORD=${PASSWORD_GITLAB}
 export GITLAB_DB_PORT=5432
 export GITLAB_DB_USER=git
@@ -37,6 +38,7 @@ export SONAR_DB_LOGIN=${SONAR_MYSQL_USER}
 export SONAR_DB_PASSWORD=${SONAR_MYSQL_PASSWORD}
 
 # Jenkins Slave
+
 export SLAVE_EXECUTORS=1
 
 # Jenkins Certificate Path
